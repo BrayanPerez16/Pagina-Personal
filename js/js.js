c3dbecd9cdbb639172457564a4dd02528030541d
Brayan Perez <brayantt138@gmail.com>
2020-06-22 19:30:47 -0500

    nuevo repositorio continuación primer avance falta información y trabajos

---
+++ js/js.js
@@ -0,0 +1,300 @@
+$(document).ready(function () {
+
+    // SideNav Initializationa
+    $(".button-collapse").sideNav();
+});
+/* fin navbar*/
+/*preload*/
+$(document).ready(function () {
+    setTimeout(function () {
+        $('body').addClass('loaded');
+
+    }, 1000);
+});
+/*progress bar*/
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra2").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=40){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra3").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=60){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra4").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=70){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra5").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=65){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra6").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=70){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra7").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra8").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=70){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra9").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=80){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra10").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=65){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra11").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=70){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra12").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=80){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra13").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=70){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra14").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=40){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra15").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=20){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra16").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra17").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra18").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra19").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=60){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra20").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra21").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=50){
+            clearInterval(intervalo);
+        }
+    },100);
+});
+
+$("#btn").click(function(){
+    var progreso = 0;
+    var intervalo = setInterval(function(){
+        progreso = progreso + 5;
+        $("#barra22").css("width", progreso +
+        "%").attr("aria-valuenow",
+        progreso).text(progreso+ "%");
+        if(progreso>=0){
+            clearInterval(intervalo);
+        }
+    },100);
+});
